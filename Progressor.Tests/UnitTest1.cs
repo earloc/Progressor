60228e6e5e8f8392f33e8c28e7fe1c287aa37530
Alexander Clare <earloc@live.de>
2016-07-05 11:41:23 +0200

    added first test

---
+++ Progressor.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Progressor.Tests {
+    [TestClass]
+    public class DobuleExtensionTests {
+        [TestMethod]
+        public void PercentOfReturns50For50PercentOf100() {
+
+            var actual = 50.PercentOf(100);
+            var expected = 50;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
