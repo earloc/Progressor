d5c04d22d349bf0a678ff31f7f7c68f6627b25c8
Alexander Clare <earloc@live.de>
2016-07-05 12:56:35 +0200

    implemented Contractors

---
+++ Progressor.Core/Contractors/ProgressInfo.cs
@@ -0,0 +1,20 @@
+﻿using Progressor.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progressor.Contractors {
+    internal class ProgressInfo<T> : IProgressInfo<T> {
+        public ProgressInfo(T item, int current, int total) {
+            
+        }
+
+        public T Item { get; }
+
+        public double Percent { get; }
+
+        public int Progress { get; }
+    }
+}
---
+++ Progressor.Core/Contractors/ProgressiveIterator.cs
@@ -0,0 +1,33 @@
+﻿using Progressor.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Progressor.Contractors;
+
+namespace Progressor.Contractors {
+    internal class ProgressiveIterator<T> : IProgressive<T> {
+
+        private readonly IEnumerable<T> _Source;
+        private readonly int _TotalCount;
+
+        public ProgressiveIterator(IEnumerable<T> source, int? totalCount = null) {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source), "must not be null");
+
+            _Source = source;
+
+            _TotalCount = totalCount ?? source.Count();
+        }
+
+        public IEnumerator<IProgressInfo<T>> GetEnumerator() {
+            return _Source.Select((x, i) => new ProgressInfo<T>(x, 0, 0)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
